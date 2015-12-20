9460b18e2c64a3ba40c57243a226c8338b66aaf4
Ewelina Olejnik <ewelina.olejnik@outlook.com>
2015-12-20 20:56:47 +0100

    Stuff

---
+++ ITConferences.Domain/Concrete/GenericRepository.cs
@@ -0,0 +1,49 @@
+﻿using ITConferences.Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITConferences.Domain.Concrete
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private IDataContext _dataContext;
+
+        public GenericRepository(IDataContext dataContext)
+        {
+            if (dataContext == null)
+                throw new ArgumentNullException("Datacontext is null");
+
+            _dataContext = dataContext;
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return _dataContext.Set<T>();
+        }
+
+        public T GetById(int id)
+        {
+            return _dataContext.Set<T>().Find(id);
+        }
+
+        public void InsertAndSubmit(T entity)
+        {
+            this._dataContext.Set<T>().Add(entity);
+            _dataContext.SaveChanges();
+        }
+
+        public void UpdateAndSubmit(T entity)
+        {
+            _dataContext.SaveChanges();
+        }
+
+        public void DeleteAndSubmit(T entity)
+        {
+            _dataContext.Set<T>().Remove(entity);
+            _dataContext.SaveChanges();
+        }
+    }
+}
---
+++ ITConferences.WebUI__/Context/ApplicationDataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ITConferences.WebUI.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace ITConferences.WebUI.Context
+{
+    public class ApplicationDataContext : IdentityDbContext<AppUser>
+    {
+        public ApplicationDataContext()
+            : base("DefaultConnection")
+        { }
+
+        public System.Data.Entity.DbSet<AppUser> AppUsers { get; set; }
+    }
+}
